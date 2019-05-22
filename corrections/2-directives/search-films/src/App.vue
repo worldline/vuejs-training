697fed41f4026785d9646d34b3bd9432cd1ff4a8
qtomasicchio <quentin.tomasicchio@gmail.com>
2019-05-22 11:39:32 +0200

    Add correction for part 2 (directives)

---
+++ corrections/2-directives/search-films/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <login-form />
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue'
+import './stylesheet.css'
+
+export default {
+  name: 'app',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
