20fa06149a03ac1435621a9f1caf3ca42b947d13
qtomasicchio <quentin.tomasicchio@gmail.com>
2019-06-03 16:21:31 +0200

    Add correction for part 6 (http)

---
+++ corrections/6-http/search-films/src/services/api.js
@@ -0,0 +1,23 @@
+import store from '@/store.js'
+
+export const BASE_URL = 'https://vue-js-backend.herokuapp.com'
+
+export async function api (url, params = {}) {
+    params = Object.assign({
+        mode: 'cors',
+        cache: 'no-cache',
+    }, params)
+
+    params.headers = Object.assign({
+        Authorization: `Bearer ${store.state.token}`,
+        'Content-Type': 'application/json'
+    }, params.headers)
+
+    let response = await fetch(BASE_URL + url, params)
+    let json = await response.json()
+    if (!response.ok){
+        let errorMessage = json && json.error ? json.error.error || json.error : response.status
+        throw new Error(errorMessage);
+    }
+    return json
+}
