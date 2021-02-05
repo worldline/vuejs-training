f273ba8534ea766732711a5550ddc6bfc214c931
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2021-02-05 18:04:51 +0100

    section routing rewritten for vue 3

--- corrections/vue3/5-routing/search-films/src/App.vue
+++ corrections/vue3/5-routing/search-films/src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <header v-if="loggedIn" id="logout-btn">
+    Connecté en tant que <b>{{user}}</b>
+    <button @click="logout">Déconnexion</button>
+  </header>
+  <router-view />
 </template>
 
 <script>
-import '@/stylesheet.css'
+import "./stylesheet.css"
+import { mapState } from "vuex"
 
 export default {
-  name: 'app'
+  name: 'App',
+  computed: mapState(['loggedIn', 'user']),
+  methods: {
+    logout(){
+      this.$store.dispatch('logout')
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ corrections/vue3/5-routing/search-films/src/views/LoginForm.vue
@@ -0,0 +1,44 @@
+<template>
+<div id="login-form">
+<form v-on:submit.prevent="onLogin">
+  <h1>Authentification</h1>
+  <p>Remplissez ce formulaire pour vous connecter.</p>
+  <hr>
+
+  <label for="email"><b>Email</b></label>
+  <input type="text" placeholder="Entrez votre courriel" id="email" name="email" required v-model="email">
+
+  <label for="psw"><b>Mot de passe</b></label>
+  <input type="password" placeholder="Entrez votre mot de passe" id="psw" name="psw" required v-model="password">
+
+  <p><button type="submit">Se connecter</button></p>
+  <p class="error" v-if="loginError">{{loginError}}</p>
+</form>
+</div>
+</template>
+
+<script>
+export default {
+    name: "LoginForm",
+    emits: ['login'],
+    data(){
+        return {
+            title: "Authentification",
+            email: "",
+            password: "",
+            loginError: null
+        }
+    },
+    methods: {
+      onLogin(){
+        if(this.email === "test@test.com" && this.password === "test1234"){
+          this.$store.dispatch('login', { user: 'test' })
+          this.$emit('login')
+          this.loginError = null
+        } else {
+          this.loginError = `Invalid email or password 🤔`
+        }
+      }
+    }
+}
+</script>
