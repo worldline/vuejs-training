d9a76d3ed32bcfd97b2d253d8f3de9332be24bcd
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-06-04 11:51:14 +0200

    use web history for vue-router

--- corrections/vue3/6-http/search-films/src/router/index.js
+++ corrections/vue3/6-http/search-films/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import { useSession } from "../stores/session.js";
 
@@ -6,7 +6,7 @@ import LoginForm from "../views/LoginForm.vue";
 import SearchFilm from "../views/SearchFilm.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: "/login",
